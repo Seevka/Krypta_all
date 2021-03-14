f2b270cbd906b59319d091c8cf858d76fb87f4d7
Маріан-Северин Шевчук <Severun2001@gmail.com>
2021-03-14 21:31:28 +0200

    Add project files.

---
+++ Krypta_all/Form2.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+namespace Krypta_all
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\Users\Sevka\source\repos\Crypta\Crypta",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog1.FileName;
+                string fileText = File.ReadAllText(filename);
+                textBox1.Text = fileText;
+            }
+            string str = textBox1.Text;
+            MessageBox.Show($"Загальна кількість символів: {textBox1.Text.Length}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            string str = textBox1.Text;
+            str = str.ToLower();
+            string str1 = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsLetter(str[i]))
+                {
+                    str1 += str[i];
+                }
+            }
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            foreach (char ch in str1)
+            {
+                if (dic.ContainsKey(ch))
+                    dic[ch]++;
+                else
+                    dic.Add(ch, 1);
+            }
+            foreach (KeyValuePair<char, int> item in dic)
+            {
+                dataGridView1.Rows.Add(item.Key, item.Value);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            string str = textBox1.Text;
+            str = str.ToLower();
+            string str1 = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsLetter(str[i]))
+                {
+                    str1 += str[i];
+                }
+            }
+            Dictionary<char, float> dic = new Dictionary<char, float>();
+            var counted = str1.GroupBy(c => c).Select(g => new { g.Key, Count = g.Count() }).OrderByDescending(o => o.Count);
+            int leng = str1.Length;
+            foreach (char ch in str1)
+            {
+                if (dic.ContainsKey(ch))
+                    dic[ch]++;
+                else
+                    dic.Add(ch, 1);
+            }
+            foreach (KeyValuePair<char, float> kvp in dic.OrderBy(key => key.Key))
+            {
+                float r = (kvp.Value / leng) * 100;
+                float r1 = (float)Math.Round(r, 1);
+                dataGridView1.Rows.Add(kvp.Key, r1);
+                //textBox2.Text += string.Format("{0}-{1}% \t", kvp.Key, r1);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, dataGridView1.Text);
+            }
+            MessageBox.Show(
+        "       Успішно збережено!",
+        ""
+        );
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+        "Бажаєте зберегти дані перед виходом?",
+        "Увага!",
+        MessageBoxButtons.YesNoCancel,
+        MessageBoxIcon.Information,
+        MessageBoxDefaultButton.Button1,
+        MessageBoxOptions.DefaultDesktopOnly);
+
+            if (result == DialogResult.Yes)
+            {
+                button1.BackColor = Color.Red;
+                SaveFileDialog sfd = new SaveFileDialog();
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(sfd.FileName, dataGridView1.Text);
+                }
+                MessageBox.Show(
+            "       Успішно збережено!",
+            ""
+            );
+                this.Hide();
+            }
+            if (result == DialogResult.No)
+            {
+                this.Hide();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            textBox1.Text = "";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Krypta_all/Form3.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+namespace Krypta_all
+{
+    public partial class Form3 : Form
+    {
+        string newstr = "";
+        char[] z;
+        char[] mas1;
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\Users\Sevka\source\repos\Crypta\Crypta",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog1.FileName;
+                string fileText = File.ReadAllText(filename);
+                textBox1.Text = fileText;
+            }
+            string str = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+            Random rnd = new Random((char)DateTime.Now.Ticks);
+            char[] mas = new char[26];
+            char[] mass = new char[26];
+            for (int i = 0; i < 26; i++)
+            {
+                char a1 = (char)rnd.Next(0x0061, 0x007B);
+                if (!mas.Contains(a1))
+                {
+                    mas[i] = a1;
+                }
+                else
+                    i--;
+            }
+
+            for (int i = 0; i < 26; i++)
+            {
+                char a11 = (char)rnd.Next(0x0041, 0x005B);
+                if (!mass.Contains(a11))
+                {
+                    mass[i] = a11;
+                }
+                else
+                    i--;
+            }
+            z = mas.Concat(mass).ToArray();
+
+            string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            mas1 = alphabet.ToCharArray();
+            //List<KeyValuePair<char, char>> dic = new List<KeyValuePair<char, char>>();
+            //for (int i = 0; i < 52; i++)
+            //{
+            //    dic.Add(new KeyValuePair<char, char>(mas1[i], z[i]));
+            //}
+            dataGridView1.Columns.Add("Before", "Before");
+            dataGridView1.Columns.Add("After", "After");
+            for (int i = 0; i < 52; i++)
+            {
+                dataGridView1.Rows.Add(mas1[i], z[i], 0);
+                //dataGridView1.DataSource = dic;
+            }
+            string str = textBox1.Text;
+            string str1="";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsLetter(str[i])||Char.IsWhiteSpace(str[i]))
+                {
+                    str1 += str[i];
+                }
+            }
+
+            char[] text1 = str1.ToCharArray();
+            bool x;
+            for (int i = 0; i < text1.Length; i++)
+            {
+                x = true;
+
+                for (int j = 0; j < mas1.Length; j++)
+                {
+                    if (text1[i] == mas1[j])
+                    {
+                        newstr += z[j];
+                        x = false;
+                    }
+                }
+                if (x)
+                {
+                    newstr += text1[i];
+                }
+            }
+            textBox2.Text = newstr;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+            char[] text2 = newstr.ToCharArray();
+            string newstr1 = "";
+            bool x;
+
+            for (int i = 0; i < text2.Length; i++)
+            {
+                x = true;
+                for (int j = 0; j < z.Length; j++)
+                {
+                    if (text2[i] == z[j])
+                    {
+                        newstr1 += mas1[j];
+                        x = false;
+                    }
+                }
+                if (x)
+                {
+                    newstr1 += text2[i];
+                }
+            }
+            textBox3.Text += newstr1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, textBox2.Text);
+            }
+            MessageBox.Show(
+        "       Успішно збережено!",
+        ""
+        );
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, textBox3.Text);
+            }
+            MessageBox.Show(
+        "       Успішно збережено!",
+        ""
+        );
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+        "Бажаєте зберегти дані перед виходом?",
+        "Увага!",
+        MessageBoxButtons.YesNoCancel,
+        MessageBoxIcon.Information,
+        MessageBoxDefaultButton.Button1,
+        MessageBoxOptions.DefaultDesktopOnly);
+
+            if (result == DialogResult.Yes)
+            {
+                button1.BackColor = Color.Red;
+                SaveFileDialog sfd = new SaveFileDialog();
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(sfd.FileName, "Зашифрований:\r\n"+textBox2.Text+"\r\n"+"Розшифрований:\r\n"+textBox3.Text);
+                }
+                MessageBox.Show(
+            "       Успішно збережено!",
+            ""
+            );
+                this.Hide();
+            }
+            if (result == DialogResult.No)
+            {
+                this.Hide();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
